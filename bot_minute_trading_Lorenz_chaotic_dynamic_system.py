d65636c0a54f938d2e8f9e9caa2747b222fa2621
Yassine Housseine <151876208+Yaskoi@users.noreply.github.com>
2024-11-12 14:25:59 +0100

    Update bot_minute_trading_Lorenz_chaotic_dynamic_system.py

--- bot_minute_trading_Lorenz_chaotic_dynamic_system.py
+++ bot_minute_trading_Lorenz_chaotic_dynamic_system.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from scipy.integrate import odeint
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 import yfinance as yf
 
 # Fonction pour simuler le système de Lorenz
@@ -28,10 +30,10 @@ x, y, z = lorenz_data.T
 asset = ['SHIB-USD']
 
 # Télécharger les données financières pour backtest
-data_1 = yf.download(asset, interval='1m', start="2024-10-11", end="2024-10-16")
-data_2 = yf.download(asset, interval='1m', start="2024-10-17", end="2024-10-24")
-data_3 = yf.download(asset, interval='1m', start="2024-10-25", end="2024-11-01")
-data_4 = yf.download(asset, interval='1m', start="2024-11-02", end="2024-11-09")
+data_1 = yf.download(asset, interval='1m', start="2024-10-14", end="2024-10-19")
+data_2 = yf.download(asset, interval='1m', start="2024-10-20", end="2024-10-27")
+data_3 = yf.download(asset, interval='1m', start="2024-10-28", end="2024-11-04")
+data_4 = yf.download(asset, interval='1m', start="2024-11-05", end="2024-11-12")
 
 data = pd.concat([data_1, data_2, data_3, data_4])
 
@@ -71,14 +73,14 @@ class MLTradingStrategy(bt.Strategy):
         idx = len(self)
         if idx >= len(self.signals):
             return  # S'assurer qu'on ne dépasse pas la taille des signaux prédits
-
-        taille_position = capital_initial / data['Close'].iloc[0]
         
+        taille_position = capital_initial / data['Close'].iloc[0]
+
         # Générer des ordres en fonction du signal
         if self.signals[idx] == 1 and not self.position:
-            self.order = self.buy(size = taille_position)
+            self.order = self.buy(size = taille_position)  # Acheter avec tout le capital disponible
         elif self.signals[idx] == 0 and self.position:
-            self.order = self.sell(size = self.position.size)
+            self.order = self.sell(size= self.position.size)  # Vendre toute la position
 
 # Initialiser Cerebro et ajouter les données et la stratégie
 cerebro = bt.Cerebro()
@@ -89,7 +91,11 @@ data_feed = bt.feeds.PandasData(dataname=data)
 cerebro.adddata(data_feed)
 
 # Ajouter un analyseur SharpeRatio
-cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='sharpe', timeframe=bt.TimeFrame.Days, riskfreerate = 0.03) # Taux actuel du Livret Jeune
+cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='sharpe', timeframe=bt.TimeFrame.Days, riskfreerate=0.03)
+
+# Maximum Drawdown pour la stratégie ML
+cerebro.addanalyzer(bt.analyzers.DrawDown, _name='drawdown')
+drawdown = results[0].analyzers.drawdown.get_analysis()['max']['drawdown']
 
 # Définir le capital de départ
 capital_initial = 100
@@ -110,3 +116,35 @@ print(f"Rendement total : {rendement:.2f} %")
 # Extraire et afficher le Sharpe Ratio
 sharpe_ratio = results[0].analyzers.sharpe.get_analysis().get('sharperatio')
 print(f"Sharpe Ratio : {sharpe_ratio:.2f}")
+
+# Maximum Drawdown pour la stratégie ML
+
+print(f"Maximum Drawdown pour la stratégie ML : {drawdown:.2f} %")
+
+# Valeur final du portefeuille pour la stratégie Buy & Hold
+rendement_bh = (data['Adj Close'].iloc[-1] - data['Adj Close'].iloc[0]) / data['Adj Close'].iloc[0] * 100
+valeur_finale_bh = capital_initial * (1 + rendement_bh / 100)
+
+print(f"Valeur finale du portefeuille en Buy & Hold: {valeur_finale_bh:.2f} USD")
+print(f"Rendement Buy & Hold : {rendement_bh:.2f} %")
+
+# Sharpe Ratio pour le modèle Buy & Hold
+rendement_bh_daily = data['Adj Close'].pct_change()[1:]  # Rendement quotidien
+
+sharpe_ratio_bh = (rendement_bh_daily.mean() / rendement_bh_daily.std()) * np.sqrt(252)  # 252 jours de trading dans une année
+
+print(f"Sharpe Ratio Buy & Hold : {sharpe_ratio_bh:.2f}")
+
+# Maximum Drawdown pour la stratégie Buy & Hold
+drawdown_bh = (data['Adj Close'].cummax() - data['Adj Close']) / data['Adj Close'].cummax() * 100
+max_drawdown_bh = drawdown_bh.max()
+
+print(f"Maximum Drawdown pour la stratégie Buy & Hold : {max_drawdown_bh:.2f} %")
+
+# Comparaison des rendements
+if rendement > rendement_bh:
+    print("La stratégie de trading basée sur le modèle de machine learning a surperformé la stratégie Buy & Hold.")
+elif rendement < rendement_bh:
+    print("La stratégie Buy & Hold a surperformé la stratégie de trading basée sur le modèle de machine learning.")
+else:
+    print("Les deux stratégies ont produit le même rendement.")
