9a1441133f6924fa25af056a89b081012ead6b4d
Yassine Housseine <151876208+Yaskoi@users.noreply.github.com>
2024-11-12 14:29:06 +0100

    Update bot_minute_trading_Mackey_Glass_chaotic_dynamic_system.py

--- bot_minute_trading_Mackey_Glass_chaotic_dynamic_system.py
+++ bot_minute_trading_Mackey_Glass_chaotic_dynamic_system.py
@@ -8,10 +8,10 @@ import yfinance as yf
 
 asset = ['SHIB-USD']
 
-data_1 = yf.download(asset, interval='1m', start="2024-10-12", end="2024-10-16")
-data_2 = yf.download(asset, interval='1m', start="2024-10-17", end="2024-10-24")
-data_3 = yf.download(asset, interval='1m', start="2024-10-25", end="2024-11-01")
-data_4 = yf.download(asset, interval='1m', start="2024-11-02", end="2024-11-09")
+data_1 = yf.download(asset, interval='1m', start="2024-10-14", end="2024-10-19")
+data_2 = yf.download(asset, interval='1m', start="2024-10-20", end="2024-10-27")
+data_3 = yf.download(asset, interval='1m', start="2024-10-28", end="2024-11-04")
+data_4 = yf.download(asset, interval='1m', start="2024-11-05", end="2024-11-12")
 
 data = pd.concat([data_1, data_2, data_3, data_4])
 
@@ -23,7 +23,7 @@ data.dropna(inplace=True)
 def mackey_glass(tau=25):
     x = [0.5]
     for t in range(1, len(data)):
-        x_t = x[-1] + 0.2 * x[-tau] / (1 + x[-tau]**15) - 0.15 * x[-1] if t >= tau else 0.5
+        x_t = x[-1] + 0.2 * x[-tau] / (1 + x[-tau]**10) - 0.1 * x[-1] if t >= tau else 0.5
         x.append(x_t)
     return np.array(x)
 
@@ -71,8 +71,12 @@ cerebro.addstrategy(MLTradingStrategy)
 data_feed = bt.feeds.PandasData(dataname=data)
 cerebro.adddata(data_feed)
 
-# Ajouter l'analyseur SharpeRatio
-cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='sharpe', timeframe=bt.TimeFrame.Days, riskfreerate=0.045)
+# Ajouter un analyseur SharpeRatio
+cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='sharpe', timeframe=bt.TimeFrame.Days, riskfreerate=0.03)
+
+# Maximum Drawdown pour la stratégie ML
+cerebro.addanalyzer(bt.analyzers.DrawDown, _name='drawdown')
+drawdown = results[0].analyzers.drawdown.get_analysis()['max']['drawdown']
 
 # Définir le capital de départ
 capital_initial = 100
@@ -91,5 +95,37 @@ print(f"Valeur finale du portefeuille : {valeur_finale:.2f} USD")
 print(f"Rendement total : {rendement:.2f} %")
 
 # Extraire et afficher le Sharpe Ratio
-sharpe_ratio = results[0].analyzers.sharpe.get_analysis()['sharperatio']
+sharpe_ratio = results[0].analyzers.sharpe.get_analysis().get('sharperatio')
 print(f"Sharpe Ratio : {sharpe_ratio:.2f}")
+
+# Maximum Drawdown pour la stratégie ML
+
+print(f"Maximum Drawdown pour la stratégie ML : {drawdown:.2f} %")
+
+# Valeur final du portefeuille pour la stratégie Buy & Hold
+rendement_bh = (data['Adj Close'].iloc[-1] - data['Adj Close'].iloc[0]) / data['Adj Close'].iloc[0] * 100
+valeur_finale_bh = capital_initial * (1 + rendement_bh / 100)
+
+print(f"Valeur finale du portefeuille en Buy & Hold: {valeur_finale_bh:.2f} USD")
+print(f"Rendement Buy & Hold : {rendement_bh:.2f} %")
+
+# Sharpe Ratio pour le modèle Buy & Hold
+rendement_bh_daily = data['Adj Close'].pct_change()[1:]  # Rendement quotidien
+
+sharpe_ratio_bh = (rendement_bh_daily.mean() / rendement_bh_daily.std()) * np.sqrt(252)  # 252 jours de trading dans une année
+
+print(f"Sharpe Ratio Buy & Hold : {sharpe_ratio_bh:.2f}")
+
+# Maximum Drawdown pour la stratégie Buy & Hold
+drawdown_bh = (data['Adj Close'].cummax() - data['Adj Close']) / data['Adj Close'].cummax() * 100
+max_drawdown_bh = drawdown_bh.max()
+
+print(f"Maximum Drawdown pour la stratégie Buy & Hold : {max_drawdown_bh:.2f} %")
+
+# Comparaison des rendements
+if rendement > rendement_bh:
+    print("La stratégie de trading basée sur le modèle de machine learning a surperformé la stratégie Buy & Hold.")
+elif rendement < rendement_bh:
+    print("La stratégie Buy & Hold a surperformé la stratégie de trading basée sur le modèle de machine learning.")
+else:
+    print("Les deux stratégies ont produit le même rendement.")
